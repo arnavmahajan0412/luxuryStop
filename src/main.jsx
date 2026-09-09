2b19ee18d2f57678710b9d412cfbde63002d7c91
Arnav Mahajan <arnavmahajan0412@gmail.com>
2026-09-10 00:37:23 +0530

    colorChange

--- src/main.jsx
+++ src/main.jsx
@@ -14,5 +14,6 @@ import './brand-carousel.css'
 import './marketplace.css'
 import './brand-logos.css'
 import './mobile-fix.css'
+import './theme-modern-heritage.css'
 import App from './App'
 createRoot(document.getElementById('root')).render(<StrictMode><App /></StrictMode>)
